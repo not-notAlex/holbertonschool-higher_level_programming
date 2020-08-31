6c320ddb0410bac400b0fa76c0907d66b43d419e
not-notAlex <alex.smith1815@gmail.com>
2020-08-31 17:58:12 +0000

    cycle answers

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *cur;
+	listint_t *fut;
+
+	if (list == NULL)
+		return (0);
+
+	cur = list;
+	fut = cur->next;
+
+	while(cur && fut)
+	{
+		if (cur == fut)
+			return (1);
+		cur = cur->next;
+		fut = fut->next;
+		if (fut != NULL)
+			fut = fut->next;
+	}
+
+	return (0);
+}
