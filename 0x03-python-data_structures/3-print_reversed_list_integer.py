9629b8de003ff05006955758218b7bed882f8e1b
not-notAlex <alex.smith1815@gmail.com>
2020-09-08 17:18:23 +0000

    fixed empty list test

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     last = len(my_list) - 1
-    while last >= 0:
-        print("{:d}".format(my_list[last]))
-        last = last - 1
+    if last > 0:
+        while last >= 0:
+            print("{:d}".format(my_list[last]))
+            last = last - 1
