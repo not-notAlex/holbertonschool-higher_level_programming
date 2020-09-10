7acd84693f41d17d03e591285cd62ec24c65fa0c
not-notAlex <alex.smith1815@gmail.com>
2020-09-10 17:12:01 +0000

    small tweak

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    return max(a_dictionary, key=a_dictionary.get)
+    if a_dictionary:
+        return sorted(a_dictionary, key=a_dictionary.get, reverse=True)
