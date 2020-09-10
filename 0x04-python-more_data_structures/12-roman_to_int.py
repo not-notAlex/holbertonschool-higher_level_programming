1a242658463a9ec4e4e86c4cc2fe12f2bece20ab
not-notAlex <alex.smith1815@gmail.com>
2020-09-10 17:26:31 +0000

    fixed prefix numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
+    if not isinstance(roman_string, str):
         return None
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    for i in roman_string:
-        if i in list(romans.keys()):
-            total += romans[i]
-    return total
+    elif roman_string:
+        romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        for i in range(0, len(roman_string)):
+            if roman_string[i] in list(romans.keys()):
+                total += romans[roman_string[i]]
+            if (i + 1) < len(roman_string):
+                if roman_string[i] == 'I' and roman_string[i + 1] == 'X':
+                    total -= 2
+                if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
+                    total -= 2
+                if roman_string[i] == 'X' and roman_string[i + 1] == 'L':
+                    total -= 20
+                if roman_string[i] == 'X' and roman_string[i + 1] == 'C':
+                    total -= 20
+                if roman_string[i] == 'C' and roman_string[i + 1] == 'D':
+                    total -= 200
+                if roman_string[i] == 'C' and roman_string[i + 1] == 'M':
+                    total -= 200
+        return total
