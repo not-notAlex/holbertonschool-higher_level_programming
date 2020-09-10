b6d6d5c432814aaa8b4d4a201cbc78ca2f2b9f7c
not-notAlex <alex.smith1815@gmail.com>
2020-09-10 17:29:02 +0000

    fixed pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return None
     elif roman_string:
-        romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
         for i in range(0, len(roman_string)):
-            if roman_string[i] in list(romans.keys()):
-                total += romans[roman_string[i]]
+            if roman_string[i] in list(rom.keys()):
+                total += rom[roman_string[i]]
             if (i + 1) < len(roman_string):
                 if roman_string[i] == 'I' and roman_string[i + 1] == 'X':
                     total -= 2
