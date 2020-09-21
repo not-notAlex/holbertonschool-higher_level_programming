1b124c54c44cbdcb685566d8638e362dd9a02736
not-notAlex <alex.smith1815@gmail.com>
2020-09-21 17:13:24 +0000

    added type error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,3 +9,6 @@ def safe_print_integer_err(value):
     except ValueError as ou:
         print("Exception: {}".format(ou), file=sys.stderr)
         return False
+    except TypeError as ou:
+        print("Exception: {}".format(ou), file=sys.stderr)
+        return False
