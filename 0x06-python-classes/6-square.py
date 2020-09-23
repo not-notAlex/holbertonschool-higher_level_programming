bda1739d6e032396d89dd6af7fecd2d9c7ff25c5
not-notAlex <alex.smith1815@gmail.com>
2020-09-23 03:28:02 +0000

    documentation and more error checks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
+
+"""
+module 6-square
+defines square class
+"""
+
+
 class Square:
+    """
+    defines square class
+    """
     def __init__(self, size=0, position=(0, 0)):
+        """
+        inits square
+        size: the size of the square
+        position: where to print the square
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """
+        returns size property
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        gives size new value
+        value: sets size to vlaue
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -19,19 +41,34 @@ class Square:
 
     @property
     def position(self):
+        """
+        returns position property
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or value[0] < 0 or value[1] < 0:
+        """
+        sets property value
+        value: new tuple for position
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not isinstance(value[0], int) \
+           or not isinstance(value[1], int) or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
     def area(self):
+        """
+        returns area of square
+        """
         return (self.__size ** 2)
 
     def my_print(self):
+        """
+        prints square to console
+        """
         for j in range(0, self.__position[1]):
             print()
         for x in range(0, self.__size):
