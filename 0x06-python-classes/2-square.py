b10ac05de123313b2e85933e33adf69d7695b732
not-notAlex <alex.smith1815@gmail.com>
2020-09-23 03:09:45 +0000

    documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+
+"""
+module 2-square
+defines a square
+"""
+
+
 class Square:
+    """
+    Defines class square
+
+    """
     def __init__(self, size=0):
+        """
+        inits square
+        size: the size of square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
