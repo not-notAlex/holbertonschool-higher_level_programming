f0a47e0abaa6ecb8d6c4ad1f229e6748b02d7f79
not-notAlex <alex.smith1815@gmail.com>
2020-09-23 03:17:44 +0000

    documentation

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python3
+
+"""
+module 4-square
+defines class square
+"""
+
+
 class Square:
+    """
+    defiens class square
+    """
     def __init__(self, size=0):
+        """
+        inits square
+        size: the size of the square
+        """
         self.size = size
 
     @property
     def size(self):
+        """
+        returns size property
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        sets size property
+        value: the new size value
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -17,4 +38,7 @@ class Square:
             self.__size = value
 
     def area(self):
+        """
+        return area of the square
+        """
         return (self.__size ** 2)
