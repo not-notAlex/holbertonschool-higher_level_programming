5462f5aba50e2ecacd2bfd3ccd451ceb83e7f193
not-notAlex <alex.smith1815@gmail.com>
2020-09-23 03:05:18 +0000

    documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square
+"""
+
+
 class Square:
+    """
+    defines class square
+    """
     def __init__(self, size):
+        """
+        size: the size of the square
+        """
         self.__size = size
