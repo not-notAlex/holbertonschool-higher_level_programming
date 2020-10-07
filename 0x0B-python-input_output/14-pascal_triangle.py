bfde0b9ab490758b21186a037f85fd4bc4258951
not-notAlex <alex.smith1815@gmail.com>
2020-10-07 22:16:27 +0000

    changed output format

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -9,6 +9,7 @@ def pascal_triangle(n):
     returns pascal triangle list
     """
     my_list = [1]
+    output = [[1]]
     if n <= 0:
         return []
     for i in range(1, n):
@@ -16,4 +17,5 @@ def pascal_triangle(n):
         for j in range(1, len(new_list)):
             my_list[j] = new_list[j] + new_list[j - 1]
         my_list.append(1)
-    return my_list
+        output.append(my_list.copy())
+    return output
