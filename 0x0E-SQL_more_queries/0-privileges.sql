57e8fa83c0d08282f380a565617f1bfd58926503
not-notAlex <alex.smith1815@gmail.com>
2020-11-17 19:42:36 +0000

    answer dump

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- prints privileges of two users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists all cities in hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+       FROM cities
+       JOIN states
+       WHERE cities.state_id = states.id
+       ORDER BY cities.id ASC;
