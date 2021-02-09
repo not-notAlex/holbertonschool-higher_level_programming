8eeebe7f0deb41dddda79823880336a3ae6eb97e
Alex Smith <1763@holbertonschool.com>
2021-02-09 19:39:11 +0000

    answer dump

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8');
+  }
+});
