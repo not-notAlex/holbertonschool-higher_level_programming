73b50c39346d1187d90ec8de2b04cc20c0d0c7e7
Alex Smith <1763@holbertonschool.com>
2021-03-22 20:15:01 +0000

    answer dump

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2], 10);
+function factorial (x) {
+  if (isNaN(x)) { return (1); }
+  if (x <= 1) { return (1); }
+  return (x * factorial(x - 1));
+}
+console.log(factorial(num));
