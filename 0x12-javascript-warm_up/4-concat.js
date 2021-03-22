fa616a43bb613fb2cb66488c2576028a160a0b34
not-notAlex <61146847+not-notAlex@users.noreply.github.com>
2021-03-22 14:59:51 -0500

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/node
-console.log(''.concat(process.argv[2], ' is ', process.argv[3]));
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const i of arr) {
-  console.log(i);
-}
