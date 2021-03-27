7de5492bac513dd255e05ae5d7811d9f8a03e616
Alex Smith <1763@holbertonschool.com>
2021-03-27 19:18:41 +0000

    fixed formatting

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     e = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+                      format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(e)
     Session = sessionmaker(bind=e)
     session = Session()
